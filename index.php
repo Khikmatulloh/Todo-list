e3afc415e7ebc6c2519ed0b8216e272bec31922b
Xikmatulloh <hikmatmahmadjonov20060826.@gmail.com>
2024-07-24 06:32:56 +0300

    change index.pk\hp

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "vendor/autoload.php";
+require 'src/Task.php';
 
 date_default_timezone_set('Asia/Tashkent');
 
@@ -10,6 +11,36 @@ if (isset($update)) {
     require 'bot/bot.php';
     return;
 }
+$tasks = new Task();
+if ($_SERVER['REQUEST_URI']=='/tasks'){
+    echo json_encode($tasks->getAll());
+    return;
+}
+$path=parse_url($_SERVER['REQUEST_URI'])['path'];
+if($path=='/add'){
+    $tasks->add($update->$text,$update->userId);
+    return;
+}
+if ($path == '/complete') {
+    $tasks->complete($update->id);
+    return;
+}
+if ($path == '/uncomplete') {
+    $tasks->uncompleted($update->id);
+    return;
+}
+
+if ($path == '/delete') {
+   $tasks->delete($update->id);
+   return;
+}
+
+
+
+
+
+
+
 
 if (count($_GET) > 0 || count($_POST) > 0) {
     $task = new Task();
--- src/Task.php
+++ src/Task.php
@@ -14,7 +14,7 @@ class Task
     public function add(string $text, int $userId): bool
     {
         $status = false;
-        $stmt   = $this->pdo->prepare("INSERT INTO todos (text, status, user_id) VALUES (:text, :status, :userId)");
+        $stmt   = $this->pdo->prepare("INSERT INTO tasks (text, status, user_id) VALUES (:text, :status, :userId)");
         $stmt->bindParam(':text', $text);
         $stmt->bindParam(':status', $status, PDO::PARAM_BOOL);
         $stmt->bindParam(':userId', $userId);
@@ -23,13 +23,13 @@ class Task
 
     public function getAll(): false|array
     {
-        return $this->pdo->query("SELECT * FROM todos")->fetchAll();
+        return $this->pdo->query("SELECT * FROM tasks")->fetchAll();
     }
 
     public function complete(int $id): bool
     {
         $status = true;
-        $stmt   = $this->pdo->prepare("UPDATE todos  SET status=:status WHERE id = :id");
+        $stmt   = $this->pdo->prepare("UPDATE tasks  SET status=:status WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->bindParam(':status', $status, PDO::PARAM_BOOL);
         return $stmt->execute();
@@ -38,7 +38,7 @@ class Task
     public function uncompleted(int $id): bool
     {
         $status = false;
-        $stmt   = $this->pdo->prepare("UPDATE todos  SET status=:status WHERE id = :id");
+        $stmt   = $this->pdo->prepare("UPDATE tasks  SET status=:status WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->bindParam(':status', $status, PDO::PARAM_BOOL);
         return $stmt->execute();
@@ -46,14 +46,14 @@ class Task
 
     public function delete(int $id): bool
     {
-        $stmt = $this->pdo->prepare("DELETE FROM todos WHERE id = :id");
+        $stmt = $this->pdo->prepare("DELETE FROM tasks WHERE id = :id");
         $stmt->bindParam(':id', $id);
         return $stmt->execute();
     }
 
     public function getTask(int $id)
     {
-        $stmt = $this->pdo->prepare("SELECT * FROM todos WHERE id = :id");
+        $stmt = $this->pdo->prepare("SELECT * FROM tasks WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_OBJ);
